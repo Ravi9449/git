9d825f71d3289d2b8e8cc2f54ca1b81c1703a437
Ravi Teja <teja.@gmail.com>
2020-12-15 23:59:43 +0530

    New Java File

---
+++ Average.java
@@ -0,0 +1,16 @@
+package com.sonata.junit;
+
+
+public class Average {
+	
+	
+	public static void main(String[] args) {
+		Average a=new Average();
+		System.out.println(a.add(30, 40));
+
+	}
+	int add(int a, int b) {	
+		return a+b;		
+	}
+	
+}
